7f2f4fa48b5ee7bc7cb3e8df062fcd898d04f8e5
Jakob Marchal <jakob.marchal@hotmail.com>
2023-04-18 14:32:45 +0200

    Add project files.

---
+++ ArkhamHorrorCompanionApp/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace ArkhamHorrorCompanionApp;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ ArkhamHorrorCompanionApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace ArkhamHorrorCompanionApp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
